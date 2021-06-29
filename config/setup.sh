b027b8eaf78c46cf6e4fdb269076d17e71fac994
Ankit Agrawal <ankitagrawal1991@gmail.com>
2021-06-29 23:24:24 +0530

    Added gitignore, zshrc, and initial setup script

---
+++ config/setup.sh
@@ -0,0 +1,43 @@
+#Update
+sudo apt update && sudo apt upgrade
+
+#Essentials
+sudo snap install vlc
+sudo apt install make
+sudo apt install vim
+sudo apt install zsh
+
+#Git and Github setup
+sudo apt install git
+git config --global user.email "ankitagrawal1991@gmail.com"
+git config --global user.name "Ankit Agrawal"
+ssh-keygen
+cat ~/.ssh/id_rsa.pub
+#gitignore
+touch ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
+
+#JDK8
+sudo apt install openjdk-8-jdk
+
+#VS Code
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+#Intellij Community
+sudo snap install intellij-idea-community --classic
+
+#Postman
+sudo snap install postman
+
+#oh-my-zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+code ~/.zshrc
+#update plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)
+source ~/.zshrc
+#Download font https://github.com/powerline/fonts/raw/master/SourceCodePro/Source%20Code%20Pro%20for%20Powerline.otf
