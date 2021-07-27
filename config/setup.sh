6d63165f127e0a6e3529224541325fe595719ce1
Ankit Agrawal <ankitagrawal1991@gmail.com>
2021-07-27 20:18:27 +0530

    Added steps to use git CLI with 2FA enabled

--- config/setup.sh
+++ config/setup.sh
@@ -18,6 +18,8 @@ git config --global user.email "ankitagrawal1991@gmail.com"
 git config --global user.name "Ankit Agrawal"
 ssh-keygen
 cat ~/.ssh/id_rsa.pub
+# Setup token for repo, needed for 2FA https://github.com/settings/tokens
+# https://docs.github.com/en/github/authenticating-to-github/keeping-your-account-and-data-secure/creating-a-personal-access-token
 #gitignore
 touch ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
